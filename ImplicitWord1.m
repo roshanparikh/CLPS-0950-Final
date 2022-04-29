60863ece110f69a6b0bdf8d344e7b901c3ebb0ca
saidimaad01 <imaad_said@brown.edu>
2022-04-29 15:02:03 -0400

    Fixation Dot and Word Display on Side of Screen

---
+++ ImplicitWord1.m
@@ -0,0 +1,149 @@
+% Edited by Imaad Said, 4/29/2022 at 1:30
+
+% Clear the workspace and the screen
+sca;
+close all;
+clear;
+
+% Here we call some default settings for setting up Psychtoolbox
+PsychDefaultSetup(2);
+
+% Seed the random number generator. Here we use the an older way to be
+% compatible with older systems. Newer syntax would be rng('shuffle').
+% For help see: help rand
+rand('seed', sum(100 * clock));
+
+% Get the screen numbers. This gives us a number for each of the screens
+% attached to our computer. For help see: Screen Screens?
+screens = Screen('Screens');
+
+% Draw we select the maximum of these numbers. So in a situation where we
+% have two screens attached to our monitor we will draw to the external
+% screen. When only one screen is attached to the monitor we will draw to
+% this. For help see: help max
+screenNumber = max(screens);
+
+% Define black and white (white will be 1 and black 0). This is because
+% luminace values are (in general) defined between 0 and 1.
+% For help see: help WhiteIndex and help BlackIndex
+white = WhiteIndex(screenNumber);
+black = BlackIndex(screenNumber);
+
+% Open the screen, defined as 1000 x 1000 matrix
+[window, windowRect] = PsychImaging('OpenWindow', 0, [255 255 255], [0 0 1000 1000], screenNumber, white, [], 32, 2);
+%Here screen is opened just for changing the SyncTest preference
+
+[screenXpixels, screenYpixels] = Screen('WindowSize', window);
+
+% Flip to clear
+Screen('Flip', window);
+
+% Query the frame duration
+ifi = Screen('GetFlipInterval', window);
+
+% Set text size to 40 
+Screen('TextSize', window, 40);
+
+% Query the maximum priority level
+topPriorityLevel = MaxPriority(window);
+
+% Get the centre coordinate of the window in pixels
+% For help see: help RectCenter
+[xCenter, yCenter] = RectCenter(windowRect);
+
+% Enable alpha blending for anti-aliasing
+% For help see: Screen BlendFunction?
+% Also see: Chapter 6 of the OpenGL programming guide
+Screen('BlendFunction', window, GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+%----------------------------------------------------------------------
+%                       Timing Information
+%----------------------------------------------------------------------
+
+% Stimulus interval time in seconds and frames
+isiTimeSecs = 1;
+isiTimeFrames = round(isiTimeSecs / ifi);
+
+
+% Number of frames to wait before next stimulus
+waitframes = 100;
+
+
+% Set the color of our dot to full red. Color is defined by red green
+% and blue components (RGB). So we have three numbers which
+% define our RGB values. The maximum number for each is 1 and the minimum
+% 0. So, "full red" is [1 0 0]. "Full green" [0 1 0] and "full blue" [0 0
+% 1]. Play around with these numbers and see the result.
+dotColor = [0 0 0];
+
+% Determine a random X and Y position for our dot. NOTE: As dot position is
+% randomised each time you run the reenript the output picture will show the
+% dot in a different position. Similarly, when you run the script the
+% position of the dot will be randomised each time. NOTE also, that if the
+% dot is drawn at the edge of the screen some of it might not be visible.
+dotXpos = xCenter
+dotYpos = yCenter
+% Dot size in pixels
+dotSizePix = 10;
+
+wordList = {'avocado', 'lemonade', 'car', 'boat'};%Defining the words to be used
+rgbColors = [1 1 1; 1 1 1; 1 1 1; 1 1 1];%defining the color RGB color code for each of the 4 words
+
+% Make the matrix which will determine our condition combinations
+condMatrixBase = [sort(repmat([1 2 3 4], 1, 3)); repmat([1 2 3 4], 1, 3)];% This matrix is 12 columns wide, allowing us to record the results of each trial
+
+
+% This is for the number of trials per condition, giving us a total of 12 trials.
+trialsPerCondition = 1;
+
+% Duplicate the condition matrix to get the full number of trials
+condMatrix = repmat(condMatrixBase, 1, trialsPerCondition);
+
+% Get the size of the matrix
+[~, numTrials] = size(condMatrix);
+
+% To randomize the conditions so that a randomly chosen word is presented in a
+% randomly chosen color
+shuffler = Shuffle(1:numTrials);
+condMatrixShuffled = condMatrix(:, shuffler);
+% Draw the dot to the screen. For information on the command used in
+% this line type "Screen DrawDots?" at the command line (without the
+% brackets) and press enter. Here we used good antialiasing to get nice
+% smooth edges
+numTrials = 5
+vbl = Screen('Flip', window);
+for trial = 1:numTrials
+
+ theWord = randi(length(wordList), 1)
+
+Screen('DrawDots', window, [dotXpos dotYpos], dotSizePix, dotColor, [], 2);
+
+% Flip to the screen. This command basically draws all of our previous
+% commands onto the screen. See later demos in the animation section on more
+% timing details. And how to demos in this section on how to draw multiple
+% rects at once.
+% For help see: Screen Flip?
+  vbl = Screen('Flip', window, vbl + (waitframes - 0.5) * ifi);
+
+% Now we have drawn to the screen we wait for a keyboard button press (any
+% key) to terminate the demo. For help see: help KbStrokeWait
+
+Screen('TextSize', window, 70);
+DrawFormattedText (window, 'red', screenXpixels * 0.25, 'center', [1 0 0]);
+
+% Flip to the screen
+  vbl = Screen('Flip', window, vbl + (waitframes - 0.5) * ifi);
+KbStrokeWait;
+
+end
+% Now we have drawn to the screen we wait for a keyboard button press (any
+% key) to terminate the demo
+
+
+% Clear the screen. "sca" is short hand for "Screen CloseAll". This clears
+% all features related to PTB. Note: we leave the variables in the
+% workspace so you can have a look at them if you want.
+% For help see: help sca
+sca;
+
+
