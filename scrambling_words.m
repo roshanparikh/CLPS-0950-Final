d639ac26b081c7a234451e198c174978af3dc9d2
roshanparikh <roshan_parikh@brown.edu>
2022-04-29 15:26:32 -0400

    Not allowing randomized words to equal themselves

--- scrambling_words.m
+++ scrambling_words.m
@@ -3,5 +3,10 @@ scrambledWords = cell(1,25);
 for i = 1:25
     inputScramble = char(wordsToScramble(1,i));
     outputScramble = randomizeStr(inputScramble);
-    scrambledWords{1,i} = outputScramble;
+    if outputScramble ~= inputScramble
+        scrambledWords{1,i} = outputScramble;
+    else 
+        outputScramble = randomizeStr(inputScramble);
+        scrambledWords{1,i} = outputScramble;
+    end
 end 
