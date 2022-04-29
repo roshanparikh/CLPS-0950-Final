09aa79e79ef8ee83e67051f556fb9924748804b0
roshanparikh <roshan_parikh@brown.edu>
2022-04-29 14:16:52 -0400

    Randomizing letters in a word

---
+++ randomizeStr.m
@@ -0,0 +1,15 @@
+%Implicit Word 2- Scrambling words, Roshan's Workspace
+function [outputStr]= randomizeStr(inputStr)
+    inputLength = length(inputStr);
+    outputStr = [];
+    outIndex = 1;
+    while inputLength > 0
+        wordIndex = randi(inputLength);
+        outputStr(outIndex) = inputStr(wordIndex);
+        inputStr(wordIndex) = [];
+        inputLength = inputLength - 1;
+        outIndex = outIndex + 1;
+    end
+    outputStr = char(outputStr)
+end
+
