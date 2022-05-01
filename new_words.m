645d764eeea5b83f23208a95eac7313f7b64225d
roshanparikh <roshan_parikh@brown.edu>
2022-05-01 18:48:34 -0400

    new word lists and new commenting

---
+++ new_words.m
@@ -0,0 +1,27 @@
+%New word lists
+newWords = cell(1,12);
+newWords{1,1} = ['t' 'a' 'b' 'l' 'e'];
+newWords{1,2} = ['l' 'a' 'b' 'o' 'r' 'a' 't' 'o' 'r' 'y'];
+newWords{1,3} = ['p' 'i' 'p' 'e' 't' 't' 'e'];
+newWords{1,4} = ['a' 't' 'o' 'm'];
+newWords{1,5} = ['s' 'a' 'x' 'o' 'p' 'h' 'o' 'n' 'e'];
+newWords{1,6} = ['h' 'a' 'i' 'r' 'c' 'u' 't'];
+newWords{1,7} = ['s' 'a' 'l' 'o' 'n'];
+newWords{1,8} = ['s' 'o' 'd' 'a'];
+newWords{1,9} = ['s' 'u' 'i' 't' 'c' 'a' 's' 'e'];
+newWords{1,10} = ['a' 'i' 'r' 'p' 'l' 'a' 'n' 'e'];
+newWords{1,11} = ['b' 'e' 'n' 'c' 'h'];
+newWords{1,12} = ['s' 'e' 'd' 'a' 'n'];
+newWords{1,13} = ['s' 'h' 'o' 'r' 't' 's'];
+newWords{1,14} = ['c' 'o' 'l' 'o' 'g' 'n' 'e'];
+newWords{1,15} = ['p' 'a' 'n' 't' 's'];
+newWords{1,16} = ['s' 'h' 'a' 'm' 'p' 'o' 'o'];
+newWords{1,17} = ['v' 'i' 't' 'a' 'm' 'i' 'n'];
+newWords{1,18} = ['t' 'i' 'r' 'e'];
+newWords{1,19} = ['p' 'h' 'o' 'n' 'e'];
+newWords{1,20} = ['l' 'a' 'n' 'g' 'u' 'a' 'g' 'e'];
+newWords{1,21} = ['t' 'r' 'e' 'e'];
+newWords{1,22} = ['s' 'y' 'r' 'u' 'p'];
+newWords{1,23} = ['s' 'u' 'r' 'v' 'e' 'y'];
+newWords{1,24} = ['d' 'o' 'c' 't' 'o' 'r'];
+newWords{1,25} = ['s' 't' 'e' 't' 'h' 'o' 's' 'c' 'o' 'p' 'e'];
---
+++ primingWordLists.m
@@ -0,0 +1,30 @@
+%Priming word lists
+inedibleWords = cell(1,12);
+inedibleWords{1,1} = ['b' 'o' 't' 't' 'l' 'e'];
+inedibleWords{1,2} = ['j' 'a' 'c' 'k' 'e' 't'];
+inedibleWords{1,3} = ['l' 'i' 'b' 'r' 'a' 'r' 'y'];
+inedibleWords{1,4} = ['p' 'a' 'p' 'e' 'r'];
+inedibleWords{1,5} = ['f' 'e' 'r' 'n' 's'];
+inedibleWords{1,6} = ['h' 'e' 'a' 't'];
+inedibleWords{1,7} = ['p' 'l' 'a' 'n' 'e' 't'];
+inedibleWords{1,8} = ['p' 'a' 'i' 'n' 't'];
+inedibleWords{1,9} = ['p' 'e' 'n'];
+inedibleWords{1,10} = ['t' 'e' 'x' 't' 'b' 'o' 'o' 'k'];
+inedibleWords{1,11} = ['c' 'o' 'm' 'p' 'u' 't' 'e' 'r'];
+inedibleWords{1,12} = ['d' 'u' 'm' 'b' 'e' 'l' 'l'];
+
+edibleWords = cell(1,13);
+edibleWords{1,1} = ['b' 'r' 'e' 'a' 'd'];
+edibleWords{1,2} = ['w' 'a' 't' 'e' 'r'];
+edibleWords{1,3} = ['a' 'v' 'o' 'c' 'a' 'd' 'o'];
+edibleWords{1,4} = ['l' 'e' 'm' 'o' 'n' 'a' 'd' 'e'];
+edibleWords{1,5} = ['a' 'u' 'b' 'e' 'r' 'g' 'i' 'n' 'e'];
+edibleWords{1,6} = ['p' 'o' 'p' 's' 'i' 'c' 'l' 'e'];
+edibleWords{1,7} = ['c' 'a' 'n' 'd' 'y'];
+edibleWords{1,8} = ['j' 'e' 'l' 'l' 'y' ];
+edibleWords{1,9} = ['p' 'a' 's' 't' 'a'];
+edibleWords{1,10} = ['b' 'u' 'r' 'g' 'e' 'r'];
+edibleWords{1,11} = ['s' 'o' 'u' 'p'];
+edibleWords{1,12} = ['t' 'o' 'm' 'a' 't' 'o'];
+edibleWords{1,13} = ['c' 'a' 'p' 's' 'i' 'c' 'u' 'm'];
+
