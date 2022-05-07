325187c909972bf61eb96faf8bc3bb76513f037c
saidimaad01 <imaad_said@brown.edu>
2022-05-07 17:45:06 -0400

    Word lists, presurvey portion

---
+++ ediblewords.m
@@ -0,0 +1,20 @@
+
+edibleWords = cell(1,13);
+edibleWords{1,1} = ['b' 'r' 'e' 'a' 'd'];
+edibleWords{1,2} = ['w' 'a' 't' 'e' 'r'];
+edibleWords{1,3} = ['a' 'v' 'o' 'c' 'a' 'd' 'o'];
+edibleWords{1,4} = ['l' 'e' 'm' 'o' 'n' 'a' 'd' 'e'];
+edibleWords{1,5} = ['a' 'u' 'b' 'e' 'r' 'g' 'i' 'n' 'e'];
+edibleWords{1,6} = ['p' 'o' 'p' 's' 'i' 'c' 'l' 'e'];
+edibleWords{1,7} = ['c' 'a' 'n' 'd' 'y'];
+edibleWords{1,8} = ['j' 'e' 'l' 'l' 'y' ];
+edibleWords{1,9} = ['p' 'a' 's' 't' 'a'];
+edibleWords{1,10} = ['b' 'u' 'r' 'g' 'e' 'r'];
+edibleWords{1,11} = ['s' 'o' 'u' 'p'];
+edibleWords{1,12} = ['t' 'o' 'm' 'a' 't' 'o'];
+edibleWords{1,13} = ['c' 'a' 'p' 's' 'i' 'c' 'u' 'm'];
+
+for i = 1:randperm(13,1)
+    edible = char((edibleWords(1,i)));
+end
+disp(edible)
---
+++ inediblewords.m
@@ -0,0 +1,18 @@
+inedibleWords = cell(1,12);
+inedibleWords{1,1} = ['b' 'o' 't' 't' 'l' 'e'];
+inedibleWords{1,2} = ['j' 'a' 'c' 'k' 'e' 't'];
+inedibleWords{1,3} = ['l' 'i' 'b' 'r' 'a' 'r' 'y'];
+inedibleWords{1,4} = ['p' 'a' 'p' 'e' 'r'];
+inedibleWords{1,5} = ['f' 'e' 'r' 'n' 's'];
+inedibleWords{1,6} = ['h' 'e' 'a' 't'];
+inedibleWords{1,7} = ['p' 'l' 'a' 'n' 'e' 't'];
+inedibleWords{1,8} = ['p' 'a' 'i' 'n' 't'];
+inedibleWords{1,9} = ['p' 'e' 'n'];
+inedibleWords{1,10} = ['t' 'e' 'x' 't' 'b' 'o' 'o' 'k'];
+inedibleWords{1,11} = ['c' 'o' 'm' 'p' 'u' 't' 'e' 'r'];
+inedibleWords{1,12} = ['d' 'u' 'm' 'b' 'e' 'l' 'l'];
+
+for i = 1:randi(12,1)
+    inedible = char(inedibleWords(1,i));
+end
+disp(inedible)
