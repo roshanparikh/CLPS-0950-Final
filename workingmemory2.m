2845ade1f001df4c5702ac12301fa022b2bb8ec2
Yasemin Emami <yasemin_emami@brown.edu>
2022-05-06 21:19:57 -0400

    Fixes/Debugging
    Fixed a mistake on the array and changed it to return to intro screen after game ends

--- workingmemory2.m
+++ workingmemory2.m
@@ -125,7 +125,7 @@ for trial = 1:numTrials
 
     % Word and color number
     wordNum = condMatrixShuffled(1, trial);
-    colorNum = condMatrixShuffled(1, trial);
+    colorNum = condMatrixShuffled(2, trial);
 
     % The color word and the color it is drawn in
     theWord = wordList(wordNum);
